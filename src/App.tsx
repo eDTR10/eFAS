4a91f36ab3e0735d61c81feca9c132cafd036021
iannico322 <iannicocaulin@gmail.com>
2024-12-03 10:26:01 +0800

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Link, Outlet } from "react-router-dom";
 
 // import { ModeToggle } from "./components/mode-toggle";
 // import Reveal from "./components/animation/reveal";
-import Logo  from '/eFAS.png'
+import Logo  from './assets/eDTR-logo.webp'
 import NavLink from "./components/link/link";
 import { ModeToggle } from "./components/mode-toggle";
 
@@ -17,24 +17,24 @@ function App() {
      <div className=" bg-background min-h-screen w-full overflow-hidden flex flex-col  items-center">
       
       <nav className=" animate__animated animate__slideInDown  z-20 bg-background fixed flex justify-between items-center w-full max-w-[1468px] py-5 border-b-[0px] border-accent \ ">
-        <Link className=" ml-5  bg-[#edf7f0]  flex items-center justify-center pb-5 pr-2 rounded-md" to="/react-vite-supreme" >
+        <Link className=" ml-5  bg-[#edf7f0]  flex items-center justify-center  rounded-md" to="/admin" >
           <img src={Logo} className="logo  w-32 object-contain " alt="Vite logo" />
         </Link>
         <nav className=" text-accent-foreground flex gap-10 uppercase items-center">
         <NavLink
-        to="/react-vite-supreme/page1"
+        to="/admin/page1"
         text="HOME"
         />
 
         <NavLink
-        to="/react-vite-supreme/page2"
+        to="/admin/page2"
         text="ABOUT US"
         />
 
       
 
         <NavLink
-        to="/react-vite-supreme/contact"
+        to="/admin/contact"
         text="Contact Us"
         />
 
